1cbfc738d53b8a62fc8c8e808ad087d9ba79391f
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-01-30 23:58:20 +0900

    Create 10992.cpp

---
+++ 별_찍기/10992.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cin >> n;
+
+	if (n == 1)
+	{
+		cout << "*\n";
+	}
+	else
+	{
+		for (int i = 0; i < n - 1; i++)
+		{
+			cout << " ";
+		}
+		cout << "*\n";
+		
+		for (int i = 0; i < n - 2; i++)
+		{
+			for (int j = 0; j < n - 2 - i; j++)
+			{
+				cout << " ";
+			}
+			cout << "*";
+
+			for (int j = 0; j < i * 2 + 1; j++)
+			{
+				cout << " ";
+			}
+			cout << "*\n";
+		}
+
+		for (int i = 0; i < 2 * n - 1; i++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
