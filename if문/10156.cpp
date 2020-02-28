a0330aeb6082f291104eca89a236b72d79d98126
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-02-28 15:40:11 +0900

    Create 10156.cpp

---
+++ if문/10156.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, n, m;
+	
+	cin >> k >> n >> m;
+
+	if (k * n <= m)
+		cout << "0\n";
+	else
+		cout << k * n - m << "\n";
+
+	return 0;
+}
