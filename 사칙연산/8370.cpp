3389607a8fe7fe9d93e7491f6cc91eea394b7e7a
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-08-20 20:14:51 +0900

    Create 8370.cpp

---
+++ 사칙연산/8370.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n1, n2, k1, k2, ans;
+    
+    cin >> n1 >> k1 >> n2 >> k2;
+    
+    ans = n1 * k1 + n2 * k2;
+    
+    cout << ans << endl;
+    
+    return 0;
+}
