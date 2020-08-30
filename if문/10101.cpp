848862efc6cd796decaca7d0515396a3e27689e9
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-08-30 20:28:21 +0900

    Create 10101.cpp

---
+++ if문/10101.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (a + b + c == 180)
+    {
+        if (a == 60 && b == 60 && c == 60)
+        {
+            cout << "Equilateral\n";
+        }
+        else if (a == b || b == c || a == c)
+        {
+            cout << "Isosceles\n";
+        }
+        else
+        {
+            cout << "Scalene\n";
+        }
+    }
+    else
+    {
+        cout << "Error\n";
+    }
+
+    return 0;
+}
