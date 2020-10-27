f60e2685086ba16569c35a2d619a82a7a4c857a3
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-10-27 19:18:14 +0900

    Create 19944.cpp

---
+++ if문/19944.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    
+    cin >> n >> m;
+    
+    if(m <= 2)
+    {
+        cout << "NEWBIE!\n";
+    }
+    else if(m <= n)
+    {
+        cout << "OLDBIE!\n";
+    }
+    else
+    {
+        cout << "TLE!\n";
+    }
+    
+    return 0;
+}
