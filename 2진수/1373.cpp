0bd10307c5af3799ca92ca327098f0067f008e69
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-11-20 12:59:35 +0900

    Update 1373.cpp

--- 2진수/1373.cpp
+++ 2진수/1373.cpp
@@ -76,3 +76,70 @@ int main()
 
 	return 0;
 }
+
+/* ----------------------------------------------------------------
+int main()
+{
+	string s;
+	int len, digit, idx, tmp;
+
+	cin >> s;
+
+	len = s.length();
+
+	if (len % 3 == 2)
+	{
+		digit = 0;
+
+		if (s[0] == '1')
+		{
+			digit += 2;
+		}
+
+		if (s[1] == '1')
+		{
+			digit += 1;
+		}
+
+		cout << digit;
+
+		idx = 2;
+	}
+	else if (len % 3 == 1)
+	{
+		digit = 0;
+
+		if (s[0] == '1')
+		{
+			digit += 1;
+		}
+
+		cout << digit;
+
+		idx = 1;
+	}
+
+	for (; idx < len;)
+	{
+		digit = 4;
+        tmp = 0;
+        
+        for(int i = 0; i < 3; i++)
+        {
+            if (s[idx] == '1')
+    		{
+                tmp += digit;
+    		}
+
+	    	idx++;
+            digit /= 2;
+        }
+
+		cout << tmp;
+	}
+
+	cout << endl;
+
+	return 0;
+}
+*/
