3e0cdbfd0a3dc5d05a7b8f766f925f811466f00b
codejin19 <codejin19@gmail.com>
2023-07-13 21:51:14 +0900

    add 1 solution

--- greedy/11501.java
+++ greedy/11501.java
@@ -2,59 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		String input = reader.readLine();
-		int len = input.length();
+    public static void main(String[] args) throws IOException {
+        //input
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(reader.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		StringBuilder sb = new StringBuilder();
+        for(int tc = 0; tc < T; tc++) {
+            int n = Integer.parseInt(reader.readLine());
+            StringTokenizer st = new StringTokenizer(reader.readLine());
+            int[] arr = new int[n];
 
-		int idx = 0;
-		int cnt = 0;
+            for(int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
 
-		for (int i = 0; i < len; i++) {
-			if (input.charAt(i) == 'X') {
-				cnt++;
-			} else {
-				if (cnt != 0) {
-					if (cnt % 2 == 0) {
-						int a = cnt / 4;
-						int b = cnt % 4;
+            int val = arr[n - 1];
+            long sum = 0;
 
-						for (int j = 0; j < a * 4; j++)
-							sb.append("A");
+            for(int i = n - 2; 0 <= i; i--) {
+                if(arr[i] < val) {
+                    sum += (val - arr[i]);
+                } else if(val < arr[i]) {
+                    val = arr[i];
+                }
+            }
 
-						for (int j = 0; j < b; j++)
-							sb.append("B");
-					} else {
-						sb = new StringBuilder();
-						sb.append("-1");
-						break;
-					}
-					
-					cnt = 0;
-				}
+            sb.append(sum).append("\n");
+        }
 
-				sb.append(".");
-			}
-		}
-
-		if (cnt != 0) {
-			if (cnt % 2 == 0) {
-				int a = cnt / 4;
-				int b = cnt % 4;
-
-				for (int j = 0; j < a * 4; j++)
-					sb.append("A");
-
-				for (int j = 0; j < b; j++)
-					sb.append("B");
-			} else {
-				sb = new StringBuilder();
-				sb.append("-1");
-			}
-		}
-		
-		System.out.println(sb);
-	}
+        System.out.print(sb.toString());
+    }
 }
