e4a4676ee9166263a1555651d3ac338a570f65ef
codejin19 <codejin19@gmail.com>
2023-10-13 22:29:57 +0900

    add 1 solution

---
+++ 출력/2393.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //input
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("  ___  ___  ___").append("\n");
+        sb.append("  | |__| |__| |").append("\n");
+        sb.append("  |           |").append("\n");
+        sb.append("   \\_________/").append("\n");
+        sb.append("    \\_______/").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |_____|").append("\n");
+        sb.append("  __/       \\__").append("\n");
+        sb.append(" /             \\").append("\n");
+        sb.append("/_______________\\").append("\n");
+
+        System.out.println(sb);
+    }
+}
